265afc0c7f1f697f0d8875c265108921933e776d
Gustavo Andrade Strunkis <59401206+gustavostrunkis@users.noreply.github.com>
2024-11-16 17:22:36 -0300

    Update zombies.js

--- projects/zombies-brazil/js/zombies.js
+++ projects/zombies-brazil/js/zombies.js
@@ -19,8 +19,8 @@ window.onload = function () {
 }
 
 function ZombiesUI() {
-    this.mapHmax = 2160;
-    this.mapWmax = 3840;
+    this.mapHmax = 900;
+    this.mapWmax = 1500;
     this.mapH = this.mapHmax;
     this.mapW = this.mapWmax;
     this.elem = 'map';
